48d15f8231b6b9d8099248ae1b800eaeb7e01745
NguyenHaiDang <nhd131004@gmail.com>
2023-10-08 09:20:30 +0700

    CSDL

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit84ede1c9321e23b65255abf52a970af4
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit84ede1c9321e23b65255abf52a970af4', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit84ede1c9321e23b65255abf52a970af4', 'loadClassLoader'));
